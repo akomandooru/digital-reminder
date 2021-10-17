0ff4a9020a279ee60d72718c3510b1782ce25f8a
Anand Komandooru <akomandooru@gmail.com>
2021-10-17 18:31:31 -0400

    Initial commit for UI fields/form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,131 @@
-import React from 'react';
+import React from "react";
+import ReactFormValidation from "react-form-input-validation";
+import "./Form.css";
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: {
+        loc:"",
+        name: "",
+        time: "",
+        dur:""
+      },
+      errors: {}
+    };
+    this.form = new ReactFormValidation(this, { locale: "en" });
+    this.form.useRules({
+      loc: "required|max:16",
+      name: "required|max:16",
+      time: "required",
+      dur: "required"|integer
+    });
+
+    this.form.onformsubmit = (fields) => {
+      console.log(fields);
+    }
+
+  }
+
+  render() {
+    return (
+        <div style={{maxWidth: "600px", margin: "0 auto"}}>
+          <h3>React Form Input Validation</h3>
+          <form
+            className="myForm"
+            noValidate
+            autoComplete="off"
+            onSubmit={this.form.handleSubmit}
+          >
+            <p>
+              <label>
+                Appointment Location
+                <input
+                  type="text"
+                  name="loc"
+                  onBlur={this.form.handleBlurEvent}
+                  onChange={this.form.handleChangeEvent}
+                  value={this.state.fields.loc}
+                  // To override the attribute name
+                  data-attribute-name="Appointment Location"
+                  data-async
+                />
+              </label>
+              <label className="error">
+                {this.state.errors.loc
+                  ? this.state.errors.loc
+                  : ""}
+              </label>
+            </p>
+
+            <p>
+              <label>
+                Customer Name
+                <input
+                  type="text"
+                  name="name"
+                  onBlur={this.form.handleBlurEvent}
+                  onChange={this.form.handleChangeEvent}
+                  value={this.state.fields.name}
+                  // To override the attribute name
+                  data-attribute-name="Customer Name"
+                  data-async
+                />
+              </label>
+              <label className="error">
+                {this.state.errors.name
+                  ? this.state.errors.name
+                  : ""}
+              </label>
+            </p>
+
+            <p>
+              <label>
+                Appointment Time
+                <input
+                  type="datetime-local"
+                  name="time"
+                  onChange={this.form.handleChangeEvent}
+                  onBlur={this.form.handleBlurEvent}
+                  value={this.state.fields.time}
+                />
+              </label>
+              <label className="error">
+                {this.state.errors.time
+                  ? this.state.errors.time
+                  : ""}
+              </label>
+            </p>
+
+            <p>
+              <label>
+                Appointment Duration
+                <input
+                  type="text"
+                  name="dur"
+                  onBlur={this.form.handleBlurEvent}
+                  onChange={this.form.handleChangeEvent}
+                  value={this.state.fields.dur}
+                  // To override the attribute name
+                  data-attribute-name="Duration"
+                  data-async
+                />
+              </label>
+              <label className="error">
+                {this.state.errors.dur
+                  ? this.state.errors.dur
+                  : ""}
+              </label>
+            </p>
+
+            <p>
+              <button type="submit">Create QR Code</button>
+            </p>
+          </form>
+        </div>
+    );
+  }
 }
 
 export default App;
